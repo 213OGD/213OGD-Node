78eeda0ca808cfba4c0d7b35102c2165423bdfc2
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-05-07 14:54:46 +0200

    webhook back

--- start_prod.sh
+++ start_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git fetch origin && git reset --hard origin/staging && git clean -f -d
+git fetch origin && git reset --hard origin/master && git clean -f -d
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
