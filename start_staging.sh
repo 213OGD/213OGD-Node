3254f167d535e1c10edf71a9e07a1bba28030cd9
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-06 20:48:22 +0200

    start staging changed

--- start_staging.sh
+++ start_staging.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/staging && git clean -f -d
-docker-compose stop
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
