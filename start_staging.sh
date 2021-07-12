ea23db9c7f9f87e665d4d32a08a8970e06294ab0
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-12 17:00:48 +0200

    set env for npm packages from github pakage registry

--- start_staging.sh
+++ start_staging.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+WORKDIR="/home/dflt/ogd213/staging/api"
 git fetch origin && git reset --hard origin/staging && git clean -f -d
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > /home/dflt/ogd213/staging/api/auth/.npmrc
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > /home/dflt/ogd213/staging/api/driv/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > $WORKDIR/auth/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > $WORKDIR/driv/.npmrc
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
