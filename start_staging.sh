6b390a85f90979eb75570384e65aeb1e8323799a
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-12 13:01:00 +0200

    set env for npm packages from github pakage registry

--- start_staging.sh
+++ start_staging.sh
@@ -1,19 +1,4 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/staging && git clean -f -d
-cd auth && touch .npmrc && echo "
-//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN
-@mohakhlf:registry=https://npm.pkg.github.com/
-username=mohakhlf
-email=mohakhellafi@gmail.com
-strict-ssl=false
-always-auth=true
-" >> .npmrc
-cd ../gdrive && touch .npmrc && echo "
-//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN
-@mohakhlf:registry=https://npm.pkg.github.com/
-username=mohakhlf
-email=mohakhellafi@gmail.com
-strict-ssl=false
-always-auth=true
-" >> .npmrc
-cd .. && GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > | tee -a auth/.npmrc gdriv/.npmrc
+GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
