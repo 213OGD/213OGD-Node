138dbe74b2b1d82439251df91f376d9d6af84cfd
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-12 13:15:15 +0200

    set env for npm packages from github pakage registry

--- start_staging.sh
+++ start_staging.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 git fetch origin && git reset --hard origin/staging && git clean -f -d
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > auth/.npmrc
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > gdriv/.npmrc
+pwd
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
