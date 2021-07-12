192ada38715d4ecff6d6152eae16319dbd129544
Mohammed <Mohammed.K@itdm-gitlab.ssii.be>
2021-07-12 17:16:32 +0200

    fix typo error npmrc

--- start_staging.sh
+++ start_staging.sh
@@ -2,5 +2,5 @@
 WORKDIR="/home/dflt/ogd213/staging/api"
 git fetch origin && git reset --hard origin/staging && git clean -f -d
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > $WORKDIR/auth/.npmrc
-echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > $WORKDIR/driv/.npmrc
+echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN\n@mohakhlf:registry=https://npm.pkg.github.com/\nusername=mohakhlf\nemail=mohakhellafi@gmail.com\nstrict-ssl=false\nalways-auth=true" > $WORKDIR/gdrive/.npmrc
 GATEWAY_PORT=$PORT docker-compose -f docker-compose.yaml -f docker-compose-prod.yaml up --build -d
