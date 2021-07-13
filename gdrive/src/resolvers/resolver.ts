e5961b00139b51faf6f05496b4243787bb7df879
S2LF <sdeuhelf@gmail.com>
2021-07-13 15:57:06 +0200

    Remove _id on File & uppercase on Tags

--- gdrive/src/resolvers/resolver.ts
+++ gdrive/src/resolvers/resolver.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any, consistent-return, no-console */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any, consistent-return */
 import { ResolverMap } from 'types/graphql-utils';
 import TagMutation, { TagQuery } from './tag.resolver';
 import { FileMutation, FileQuery } from './file.resolver';
@@ -7,6 +8,21 @@ const resolvers: ResolverMap = {
   Query: {
     ...FileQuery,
     ...TagQuery,
+  },
+  File: {
+    id: ({ _id }) => {
+      return _id;
+    },
+    tags: ({ tags }) => {
+      return tags.map((name: string) => {
+        return name.charAt(0).toUpperCase() + name.slice(1);
+      });
+    },
+  },
+  Tag: {
+    name: ({ name }) => {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    },
   },
   Mutation: {
     ...TagMutation,
